6be194ebcd62b4cb8b895255bf1806ea1885057e
UMESH THAKUR <67777248+umeshxthakur@users.noreply.github.com>
2022-03-30 00:24:53 +0530

    Add files via upload

---
+++ Quote generator/script.js
@@ -0,0 +1,26 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy");
+
+function ramdomQuote(){
+
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result=>{
+        console.log(result);
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+
+    });
+}
+
+soundBtn.addEventListener("click",()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+quoteBtn.addEventListener("click",ramdomQuote);
